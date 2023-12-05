140f01e802d5ea982adef4284639e8dc4d318bf9
echo <echoz0724@gmail.com>
2023-12-04 19:30:05 -0500

    sss

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors'
-app.use(cors({
-    origin: '*'
-}));
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
 
 const port = process.env.PORT || 3001;
 
